a05921e65694809a1af95a4c1311a854d8c5e29c
gusutaharu <j.gusutahu.arufonnsu@gmail.com>
2024-11-03 22:24:12 +0900

    lesson07

--- js/main.js
+++ js/main.js
@@ -4,22 +4,16 @@
   function createColumn(col){
     const source = [];
     for (let i = 0; i < 15; i++) {
-      //1...15
-      //source[i] = i + 1 + 15 * 0;
-      //16...30
-      //source[i] = i + 1 + 15 * 1;
-      //31...45
-      //source[i] = i + 1 + 15 * 2;
       source[i] = i + 1 + 15 * col;
     }
 
-    const b = [];
+    const column = [];
 
     for (let i = 0; i < 5; i++){
-      b[i] = source.splice(Math.floor(Math.random() * source.length), 1)[0];
+      column[i] = source.splice(Math.floor(Math.random() * source.length), 1)[0];
     }
 
-    console.log(b);
+    return column;
   }
 
   const columns = [];
@@ -28,6 +22,7 @@
   columns[2] = createColumn(2);
   columns[3] = createColumn(3);
   columns[4] = createColumn(4);
+  columns[2][2] = 'FREE';
 
-
+  console.table(columns);
 }
